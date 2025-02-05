94239bb40226c19b3cb8a0bfcb459d467d1ee9db
Artem Vasilev <prometey1982@mail.ru>
2025-02-05 15:16:09 +0300

    Added support for float datatype in logger

--- Logger/src/LogParameter.cpp
+++ Logger/src/LogParameter.cpp
@@ -1,63 +1,80 @@
 #include "logger/LogParameter.hpp"
 
 namespace {
-double processSign(uint32_t value, bool isSigned, size_t size) {
-  if (isSigned) {
-    switch (size) {
-    case 1:
-      return static_cast<int8_t>(value);
-    case 2:
-      return static_cast<int16_t>(value);
-    default:
-      return static_cast<int32_t>(value);
-    }
-  } else {
-    return value;
-  }
-}
+	double processSign(uint32_t value, bool isSigned, size_t size) {
+		if (isSigned) {
+			switch (size) {
+			case 1:
+				return static_cast<int8_t>(value);
+			case 2:
+				return static_cast<int16_t>(value);
+			default:
+				return static_cast<int32_t>(value);
+			}
+		}
+		else {
+			return value;
+		}
+	}
 } // namespace
 
 namespace logger {
-LogParameter::LogParameter(const std::string &name, uint32_t addr, size_t size,
-                           uint32_t bitmask, const std::string &unit,
-                           bool isSigned, bool isInverseConversion,
-                           double factor, double offset,
-                           const std::string &description)
-    : _name{name}, _addr{addr}, _size{size}, _bitmask{bitmask}, _unit{unit},
-      _isSigned{isSigned}, _isInverseConversion{isInverseConversion},
-      _factor{factor}, _offset{offset}, _description{description} {}
+	LogParameter::LogParameter(const std::string& name, uint32_t addr, size_t size,
+		DataType dataType, uint32_t bitmask, const std::string& unit,
+		bool isSigned, bool isInverseConversion,
+		double factor, double offset,
+		const std::string& description)
+		: _name{ name }, _addr{ addr }, _size{ size }, _dataType{ dataType },
+		_bitmask{ bitmask }, _unit{ unit }, _isSigned{ isSigned },
+		_isInverseConversion{ isInverseConversion }, _factor{ factor },
+		_offset{ offset }, _description{ description }
+	{
+	}
 
-const std::string &LogParameter::name() const { return _name; }
+	const std::string& LogParameter::name() const { return _name; }
 
-uint32_t LogParameter::addr() const { return _addr; }
+	uint32_t LogParameter::addr() const { return _addr; }
 
-size_t LogParameter::size() const { return _size; }
+	size_t LogParameter::size() const { return _size; }
 
-uint32_t LogParameter::bitmask() const { return _bitmask; }
+	DataType LogParameter::dataType() const { return _dataType; }
 
-const std::string &LogParameter::unit() const { return _unit; }
+	uint32_t LogParameter::bitmask() const { return _bitmask; }
 
-const bool LogParameter::isSigned() const { return _isSigned; }
+	const std::string& LogParameter::unit() const { return _unit; }
 
-const bool LogParameter::isInverseConversion() const {
-  return _isInverseConversion;
-}
+	const bool LogParameter::isSigned() const { return _isSigned; }
 
-double LogParameter::factor() const { return _factor; }
+	const bool LogParameter::isInverseConversion() const {
+		return _isInverseConversion;
+	}
 
-double LogParameter::offset() const { return _offset; }
+	double LogParameter::factor() const { return _factor; }
 
-const std::string &LogParameter::description() const { return _description; }
+	double LogParameter::offset() const { return _offset; }
 
-double LogParameter::formatValue(uint32_t value) const {
-  if (_bitmask) {
-    value &= _bitmask;
-  }
-  if (_isInverseConversion) {
-    return _factor / (processSign(value, _isSigned, _size) + _offset);
-  } else {
-    return processSign(value, _isSigned, _size) * _factor + _offset;
-  }
-}
+	const std::string& LogParameter::description() const { return _description; }
+
+	static float ieee_float(uint32_t f) {
+		static_assert(sizeof(float) == sizeof f, "`float` has a weird size.");
+		float ret;
+		std::memcpy(&ret, &f, sizeof(float));
+		return ret;
+	}
+
+	double LogParameter::formatValue(uint32_t value) const {
+		if (_dataType == DataType::Float) {
+			return ieee_float(value);
+		}
+		if (_bitmask) {
+			value &= _bitmask;
+		}
+		if (_isInverseConversion) {
+			return _factor / (processSign(value, _isSigned, _size) + _offset);
+		}
+		else {
+			return processSign(value, _isSigned, _size) * _factor + _offset;
+		}
+	}
 
 } // namespace logger
--- Logger/src/LogParameters.cpp
+++ Logger/src/LogParameters.cpp
@@ -1,5 +1,7 @@
 #include "logger/LogParameters.hpp"
 
+#include <common/Util.hpp>
+
 #pragma warning(push)
 #pragma warning(disable : 4996)
 #include "../fast-cpp-csv-parser/csv.h"
@@ -9,50 +11,58 @@
 
 namespace logger {
 
-template <typename Reader> void LogParameters::load(Reader &reader) {
-  std::string name;
-  std::string addr;
-  size_t size;
-  std::string bitmask;
-  std::string unit;
-  uint8_t isSigned;
-  uint8_t isInverseConversion;
-  double factor;
-  double offset;
-  std::string description;
-  reader.read_header(io::ignore_extra_column, "Name", "Address", "Size",
-                     "Bitmask", "Unit", "Signed", "I", "Factor", "Offset",
-                     "Comment");
-  while (reader.read_row(name, addr, size, bitmask, unit, isSigned,
-                         isInverseConversion, factor, offset, description)) {
-    _parameters.emplace_back(name, std::stoul(addr, nullptr, 16), size,
-                             std::stoul(bitmask, nullptr, 16), unit,
-                             (isSigned > 0), (isInverseConversion > 0), factor,
-                             offset, description);
-  }
-}
+	DataType getDataType(const std::string& input) {
+		if (common::toLower(input) == "f")
+			return DataType::Float;
 
-LogParameters::LogParameters(const std::string &path) {
-  io::CSVReader<10> reader{path};
-  load(reader);
-}
+		return DataType::Int;
+	}
 
-LogParameters::LogParameters(std::istream &stream) {
-  io::CSVReader<10> reader{"log.params", stream};
-  load(reader);
-}
+	template <typename Reader> void LogParameters::load(Reader& reader) {
+		std::string name;
+		std::string addr;
+		size_t size;
+		std::string bitmask;
+		std::string unit;
+		std::string dataType;
+		uint8_t isSigned;
+		uint8_t isInverseConversion;
+		double factor;
+		double offset;
+		std::string description;
+		reader.read_header(io::ignore_extra_column | io::ignore_missing_column, "Name", "Address", "Size", "DataType",
+			"Bitmask", "Unit", "Signed", "I", "Factor", "Offset",
+			"Comment");
+		while (reader.read_row(name, addr, size, dataType, bitmask, unit, isSigned,
+			isInverseConversion, factor, offset, description)) {
+			_parameters.emplace_back(name, std::stoul(addr, nullptr, 16), size,
+				getDataType(dataType), std::stoul(bitmask, nullptr, 16), unit,
+				(isSigned > 0), (isInverseConversion > 0), factor,
+				offset, description);
+		}
+	}
 
-const LogParameters &LogParameters::operator=(const LogParameters &rhs) {
-  if (this == &rhs)
-    return *this;
+	LogParameters::LogParameters(const std::string& path) {
+		io::CSVReader<11> reader{ path };
+		load(reader);
+	}
 
-  _parameters = rhs._parameters;
+	LogParameters::LogParameters(std::istream& stream) {
+		io::CSVReader<11> reader{ "log.params", stream };
+		load(reader);
+	}
 
-  return *this;
-}
+	const LogParameters& LogParameters::operator=(const LogParameters& rhs) {
+		if (this == &rhs)
+			return *this;
 
-const std::vector<LogParameter> &LogParameters::parameters() const {
-  return _parameters;
-}
+		_parameters = rhs._parameters;
+
+		return *this;
+	}
+
+	const std::vector<LogParameter>& LogParameters::parameters() const {
+		return _parameters;
+	}
 
 } // namespace logger
