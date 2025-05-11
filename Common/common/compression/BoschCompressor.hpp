7b3014de5399fd392c88f6ac0fefbb12b3b88354
Artem Vasilev <prometey1982@mail.ru>
2025-05-11 16:37:01 +0300

    Added Bosch compression algorithm

---
+++ Common/common/compression/BoschCompressor.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CompressorBase.hpp"
+
+namespace common {
+
+class BoschCompressor: public CompressorBase {
+public:
+    virtual std::vector<uint8_t> compress(const std::vector<uint8_t>& input) override;
+    virtual std::vector<uint8_t> decompress(const std::vector<uint8_t>& input) override;
+};
+
+} // namespace common
